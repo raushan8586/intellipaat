11612b73572bdc8641fc89adacdc37b2ef4e60e8
raushan8586 <72825046+raushan8586@users.noreply.github.com>
2023-01-20 07:48:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -32,6 +32,40 @@ resource "aws_subnet" "intellipaat-subnet" {
   }
 }
 
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.intellipaat-vpc.id
+
+  tags = {
+    Name = "intellipaat_igw"
+  }
+}
+
+resource "aws_security_group" "intellipaat-sg" {
+  name        = "intellipaat-sg"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.intellipaat-vpc.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "intellipaat-sg"
+  }
+}
+
 
 
 
